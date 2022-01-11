684f71e305414160249620c9abe689dc9158607f
AdriHappies <adrifegar97@gmail.com>
2022-01-11 18:35:59 +0100

    commit

---
+++ MvcCoreCrudDepartamentosEF/Controllers/EmpleadosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCrudDepartamentosEF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCrudDepartamentosEF.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcCoreCrudDepartamentosEF/Repositories/RepositoryEmpleados.cs
+++ MvcCoreCrudDepartamentosEF/Repositories/RepositoryEmpleados.cs
@@ -23,8 +23,30 @@ namespace MvcCoreCrudDepartamentosEF.Repositories
 
         public List<string> GetOficios()
         {
-            var consulta = from datos in this.context.Empleados select datos.Oficio;
-            
+            var consulta = (from datos in this.context.Empleados select datos.Oficio).Distinct();
+            return consulta.ToList();
+        }
+
+        //consulta para devolver los empleados por oficio
+        public List<Empleado> GetEmpleadosOficio(string oficio)
+        {
+            var consulta = from datos in this.context.Empleados
+                           where datos.Oficio == oficio
+                           select datos;
+            return consulta.ToList();
+        }
+
+        //consulta para modificar el salario de los empleados por oficio
+        public void IncrementarSalarioEmpleados(string oficio, int incremento)
+        {
+            //buscar los empleados a modificar
+            List<Empleado> empleados = this.GetEmpleadosOficio(oficio);
+            //debemos recorrer todos los empleados y modificar el salario de forma individual
+            foreach (Empleado emp in empleados)
+            {
+                emp.Salario += incremento;
+            }
+            this.context.SaveChanges();
         }
     }
 }
